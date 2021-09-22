72e884c9f42f26f8ee0f7d79b6c89b43b8b2a813
theekshanamadumal <66960247+theekshanamadumal@users.noreply.github.com>
2021-09-22 12:58:07 +0530

    manager list

--- frontend/src/pages/itAdmin/management/management.jsx
+++ frontend/src/pages/itAdmin/management/management.jsx
@@ -1,85 +1,98 @@
-import { useState } from "react";
 import "./management.css";
 import { Link } from "react-router-dom";
-import React from "react";
-import { DataGrid } from "@material-ui/data-grid";
-import { rows } from "../../../dataCollection";
-import { Delete } from "@material-ui/icons";
-export default function Management() {
-  const [data, setData] = useState(rows);
+import React, { Component } from "react";
+import axios from "axios";
+import { Delete, SportsMma } from "@material-ui/icons";
 
-  const handleDelete = (id) => {
-    setData(data.filter((item) => item.id !== id));
-  };
+import { Box } from "@material-ui/core";
+import SalesPersonList from "../../../components/salesPersonComp/salesPesonListComp";
+import SalesPersonToolBar from "../../../components/salesPersonComp/salesPersonToolbar";
 
-  const columns = [
-    {
-      field: "id",
-      headerName: "ID",
-      width: 90,
-    },
-    {
-      field: "fullName",
-      headerName: "Full Name",
-      width: 200,
-      editable: true,
-      renderCell: (params) => {
-        return (
-          <div>
-            <span>{params.row.fullName}</span>
-          </div>
-        );
-      },
-    },
-    {
-      field: "email",
-      headerName: "Email",
-      width: 300,
-      editable: true,
-    },
-    {
-      field: "phoneNo",
-      headerName: "Phone Number",
-      width: 200,
-      editable: true,
-    },
-    {
-      field: "action",
-      headerName: "Action",
-      width: 160,
-      renderCell: (params) => {
-        return (
-          <div className="actionButton">
-            <Link to={"/itAdmin/management/" + params.row.id}>
-              <button className="editButton">Edit</button>
-            </Link>
+const Person = (props) => (
+  <tr>
+    <td>{props.salesperson.idNumber}</td>
 
-            <Delete
-              className="deleteButton"
-              onClick={() => handleDelete(params.row.id)}
-            />
-          </div>
-        );
-      },
-    },
-  ];
+    <td>
+      {props.salesperson.firstName}
+      {props.salesperson.lastName}
+    </td>
+    <td>{props.salesperson.password}</td>
+    <td>{props.salesperson.address}</td>
+    <td>{props.salesperson.city}</td>
+    <td>{props.salesperson.phoneNumber}</td>
+    <td>{props.salesperson.email}</td>
+    <td>{props.salesperson.joinedDate.substring(0, 10)}</td>
+    <td>
+      <Link to={"/management/salesPerson/" + props.salesperson._id}>edit</Link>{" "}
+      |{" "}
+      <a
+        href="#"
+        onClick={() => {
+          props.handleDelete(props.salesperson.idNumber);
+        }}
+      >
+        delete
+      </a>
+    </td>
+  </tr>
+);
 
-  return (
-    <div className="salesPerson">
-      <div className="contain ">
-        <h1 className="heading">Management List</h1>
-        <Link to="/itAdmin/addManager">
-          <button className="addUser">Add New Salesperson</button>
-        </Link>
+export default class SalesPerson extends Component {
+  //const [data, setData] = useState(rows);
+  constructor(props) {
+    super(props);
+
+    this.handleDelete = this.handleDelete.bind(this);
+    this.state = {
+      salespersons: [],
+    };
+  }
+  componentDidMount() {
+    axios
+      .get("http://localhost:3001/itAdmin/management")
+      .then((response) => {
+        this.setState({ salespersons: response.data });
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+  handleDelete(id) {
+    //setData(data.filter((item) => item.id !== id));
+    axios
+      .delete("http://localhost:5000/itAdmin/management/" + id)
+      .then((response) => {
+        console.log(response.data);
+      });
+
+    this.setState({
+      exercises: this.state.salespersons.filter((el) => el._id !== id),
+    });
+  }
+
+  salespersonsList() {
+    console.log(this.salespersonsList);
+    return this.state.salespersons.map((currentsalesperson) => {
+      return (
+        <Person
+          salesperson={currentsalesperson}
+          deleteExercise={this.handleDelete}
+          key={currentsalesperson.idNumber}
+        />
+      );
+    });
+  }
+
+  render() {
+    return (
+      <div className="salesPerson">
+        <SalesPersonToolBar className="contain" />
+        <Box sx={{ pt: 3 }} className="contain">
+          <SalesPersonList salesPerson={this.state.salespersons} />
+        </Box>
       </div>
-      <DataGrid
-        rows={data}
-        columns={columns}
-        pageSize={10}
-        checkboxSelection
-        disableSelectionOnClick
-        className="container grid"
-      />
-    </div>
-  );
+    );
+  }
 }
