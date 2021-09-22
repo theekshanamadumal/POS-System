0f00ee380fe6f18edac4f82d6faef9a36a9dca78
theekshanamadumal <66960247+theekshanamadumal@users.noreply.github.com>
2021-09-22 10:04:06 +0530

    react dependancies

--- frontend/src/pages/management/Orders/orders.jsx
+++ frontend/src/pages/management/Orders/orders.jsx
@@ -1,20 +1,17 @@
-import React from 'react';
+import React from "react";
 import "./order.css";
-import {orderRows} from "../../../dataCollection";
-import { Box } from '@material-ui/core';
-import OrderList from '../../../components/orderComp/orderList';
+import { orderRows } from "../../../dataCollection";
+import { Box } from "@material-ui/core";
+import OrderList from "../../../components/orderComp/orderList";
 import OrderToolBar from "../../../components/orderComp/orderToolbar";
-  
-  export default function Order() {
-    return (
-        <div className="order">
-            <OrderToolBar className="contain"/>
-              <Box sx={{ pt: 3 }} className="contain">
-                  <OrderList orders={orderRows} />
-              </Box>
-            
-            
-  
-        </div>
-    )
-  }
+
+export default function Order() {
+  return (
+    <div className="order">
+      <OrderToolBar className="contain" />
+      <Box sx={{ pt: 3 }} className="contain">
+        <OrderList orders={orderRows} />
+      </Box>
+    </div>
+  );
+}
