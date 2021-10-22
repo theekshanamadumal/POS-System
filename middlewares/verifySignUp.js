73f49a701829bb332adfd11743ac263c1d77cfbf
SahanSamarakoon <sahan.18@cse.mrt.ac.lk>
2021-10-22 22:39:18 +0530

    22/10/2021 Fixed User - Sahan

--- middlewares/verifySignUp.js
+++ middlewares/verifySignUp.js
@@ -5,9 +5,9 @@ const User = db.user;
 //Middleware funtions for Signup procces
 
 checkDuplicateUsernameOrEmail = (req, res, next) => {
-  // Username
+  // Check Email
   User.findOne({
-    username: req.body.username
+    email: req.body.email
   }).exec((err, user) => {
     if (err) {
       res.status(500).send({ message: err });
@@ -15,26 +15,9 @@ checkDuplicateUsernameOrEmail = (req, res, next) => {
     }
 
     if (user) {
-      res.status(400).send({ message: "Failed! Username is already in use!" });
+      res.status(400).send({ message: "Failed! Email is already in use!" });
       return;
     }
-
-    // Email
-    User.findOne({
-      email: req.body.email
-    }).exec((err, user) => {
-      if (err) {
-        res.status(500).send({ message: err });
-        return;
-      }
-
-      if (user) {
-        res.status(400).send({ message: "Failed! Email is already in use!" });
-        return;
-      }
-
-      next();
-    });
   });
 };
 
