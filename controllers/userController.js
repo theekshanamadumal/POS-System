5fc3e2aa08e34c8bf883b0b94df4667ee0109b9b
SahanSamarakoon <sahan.18@cse.mrt.ac.lk>
2021-11-12 01:48:36 +0530

    Final - Salesperson APIs

--- controllers/userController.js
+++ controllers/userController.js
@@ -86,17 +86,4 @@ module.exports =  class UserController {
 
                         
     }
-  
-//Test funtions to check authorization
-exports.admin = (req, res) => {
-    res.status(200).send("Admin Content.");
-};
-
-exports.manager = (req, res) => {
-    res.status(200).send("Manager Content.");
-};
-
-exports.salesperson = (req, res) => {
-    res.status(200).send("Salesperson Content.");
-};
-
+}
